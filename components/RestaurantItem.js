f08f7e1b7971c7ee35cef77deaa1f7c806981a5f
Sebastian <sebastianwebp@gmail.com>
2021-12-14 15:08:18 -0500

    made restaurant component, it now dynamically updates data

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -1,12 +1,12 @@
 import React from "react"
 import {View, Text, Image, TouchableOpacity} from "react-native"
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+import MaterialCommunityIcons from "react-native-vector-icons/Ionicons"
 
 const localRestaurants = [
   {
     name: "Beachside Bar",
     image_url:
-      "https://static.onecms.io/wp-content/uploads/sites/9/2020/04/24/ppp-why-wont-anyone-rescue-restaurants-FT-BLOG0420.jpg",
+      "https://media.istockphoto.com/photos/empty-restaurant-interior-picture-id1290237592?b=1&k=20&m=1290237592&s=170667a&w=0&h=fgXWrrQ7qWpbiO8O_dpEVlS4JsTZYH8e3FoZ4UeoQH8=",
     categories: ["Cafe", "Bar"],
     price: "$$",
     reviews: 1244,
@@ -24,7 +24,7 @@ const localRestaurants = [
   {
     name: "India's Grill",
     image_url:
-      "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      "https://media.istockphoto.com/photos/very-stylish-indian-gourmet-restaurant-picture-id1163284610?b=1&k=20&m=1163284610&s=170667a&w=0&h=jC4tmPenCZc5WFY3ix8NgJZFzveHUGuSKRP-zWhKerY=",
     categories: ["Indian", "Bar"],
     price: "$$",
     reviews: 700,
@@ -35,60 +35,57 @@ const localRestaurants = [
 export default function RestaurantItem() {
   return (
     <TouchableOpacity activeOpacity={1} style={{marginBottom: 30}}>
-      {localRestaurants.map(restaurant,index) => (
-        <View key={index} style={{marginTop: 10, padding: 15, backgroundColor: "white"}}>
-        <RestaurantImage image={restaurant.image} />
-        <RestaurantInfo name={restaurant.name} rating={restaurant.rating}
-        />
-      </View>
+      {localRestaurants.map((restaurant, index) => (
+        <View
+          key={index}
+          style={{marginTop: 10, padding: 15, backgroundColor: "white"}}
+        >
+          <RestaurantImage image={restaurant.image_url} />
+          <RestaurantInfo name={restaurant.name} rating={restaurant.rating} />
+        </View>
+      ))}
     </TouchableOpacity>
-      )
-    }
-  )     
+  )
 }
 
-const RestaurantImage = () => (
-  <>
+const RestaurantImage = (props) => (
+  <TouchableOpacity activeOpacity={1} style={{marginBottom: 30}}>
     <Image
       source={{
-        uri: "https://www.bbrollingdoor.com/upload/services/mainimages/Restuarant1.jpg",
+        uri: props.image,
       }}
       style={{width: "100%", height: 180}}
     />
     <TouchableOpacity style={{position: "absolute", right: 20, top: 20}}>
       <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
     </TouchableOpacity>
-  </>
+  </TouchableOpacity>
 )
 
-const RestaurantInfo = () => (
-  <>
+const RestaurantInfo = (props) => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text style={{fontSize: 15, fontWeight: "bold"}}>{props.name}</Text>
+      <Text style={{fontSize: 13, color: "gray"}}>30-45 min •</Text>
+    </View>
     <View
       style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
+        backgroundColor: "white",
+        height: 30,
+        width: 30,
         alignItems: "center",
-        marginTop: 10,
+        justifyContent: "center",
+        borderRadius: 15,
       }}
     >
-      <View>
-        <Text style={{fontSize: 15, fontWeight: "bold"}}>
-          Farmhouse Kitchen Thai Cuisine
-        </Text>
-        <Text style={{fontSize: 13, color: "gray"}}>30-45 • min </Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "#eee",
-          height: 30,
-          width: 30,
-          alignItems: "center",
-          justifyContent: "center",
-          borderRadius: 15,
-        }}
-      >
-        <Text>4.5</Text>
-      </View>
+      <Text>{props.rating}</Text>
     </View>
-  </>
+  </View>
 )
